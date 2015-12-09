e8bc142b765fcdac2acddd26ec485204cfb93267
JNMarcos <marcos_joaos@hotmail.com>
2015-12-08 21:15:20 -0300

    NovaFunçãoPréRequisitoOK sem uso do curso como está no banco

--- ProjetoBanco/NovaFunçãoPréRequisitoOK.sql
+++ ProjetoBanco/NovaFunçãoPréRequisitoOK.sql
@@ -1,52 +1,46 @@
--- FUNÇÕES DE MYSQL --
-
 delimiter |
 -- * (não usado id/cpf... para evitar confusão no código com a coluna da tabela de mesmo nome)
 
 -- passa o codCurso* , idDisciplina e cpfAluno
 -- ele procura ba tabela prerequisito quando o codCurso e  idDisciplina são iguais
-create function verificarSePreRequisitoCumprido (codCurso int, idDisciplina int, cpfAluno int) returns boolean not deterministic
+create function verificarSePreRequisitoCumprido (idDisciplina int, cpfAluno int) returns boolean not deterministic
 	begin
 		declare done int default 0;
-		declare idC int; --  cursoLinha
 		declare idD int; -- disciplinaLinha
 		declare idPre int; -- préRequisitoLinha
         declare preReqOK boolean;
         declare tudoOK boolean default true;
-		declare cursoCursor cursor for select idCurso, dependente, requesito from preRequisito; 
+		declare preCursor cursor for select dependente, requesito from preRequisito; 
 		declare continue handler for not found set done = 1;
 
-		open cursoCursor;
+		open preCursor;
 		repeat
-			fetch cursoCursor into idC, idD, idPre;
-			if (idC = codCurso and idD = idDisciplina) then
-			set preReqOK =  verificarSePreRequisitoCumpridoOferta(idC, idPre, cpfAluno); -- idPre não idD porque vai verificar se o pré-requsito está ok
+			fetch cursoCursor into idD, idPre;
+			if (idD = idDisciplina) then
+			set preReqOK =  verificarSePreRequisitoCumpridoOferta(idPre, cpfAluno); -- idPre não idD porque vai verificar se o pré-requsito está ok
             set tudoOK = tudoOK and preReqOK;
             end if;
 			until done
 		end repeat;
-        close cursoCursor;
+        close preCursor;
         return tudoOK;
 	end
 	|
-
-
-create function verificarSePreRequisitoCumpridoOferta (codCurso int, idDisc int, idAl int) returns boolean not deterministic
+create function verificarSePreRequisitoCumpridoOferta (idDisc int, cpfAl int) returns boolean not deterministic
 	begin
 		declare done int default 0;
-		declare idC int; -- cursoLinha
 		declare idD int; -- disciplinaLinha
         declare idO int; -- ofertaLinha
         declare alunoPassado boolean default false;
         declare cond varchar(25);
-		declare ofertaCursor cursor for select idCurso, idDisciplina, idOferta from ofertaDisciplina; -- ajeitar lá na tabela, está prerequEsito
+		declare ofertaCursor cursor for select idDisciplina, idOferta from ofertaDisciplina; -- ajeitar lá na tabela, está prerequEsito
 		declare continue handler for not found set done = 1;
         
         open ofertaCursor;
 		repeat
-			fetch ofertaCursor into idC, idD, idO;
-			if (idC = codCurso and idD = idDisc) then
-				set cond = verificarSePreRequisitoCumpridoHistorico(idAl, idO);
+			fetch ofertaCursor into idD, idO;
+			if (idD = idDisc) then
+				set cond = verificarSePreRequisitoCumpridoHistorico(cpfAl, idO);
 				if (cond = 'aprovado') then
                  	set alunoPassado = true; 
 					set done = 1;
@@ -59,20 +53,20 @@ create function verificarSePreRequisitoCumpridoOferta (codCurso int, idDisc int,
 	end
 |
 
-create function verificarSePreRequisitoCumpridoHistorico (idAl int, idOf int) returns varchar(25) not deterministic-- ver o histórico
+create function verificarSePreRequisitoCumpridoHistorico (cpfAl int, idOf int) returns varchar(25) not deterministic-- ver o histórico
 	begin
 		declare done int default 0;
-		declare idA int; -- alunoLinha
+		declare cpfA int; -- alunoLinha
 		declare idO int; -- ofertaLinha
 		declare idC varchar(25); -- condiçãoLinha
-        declare passadoCondicao varchar(25) default 'reprovado';
+        declare passadoCondicao varchar(25) default 'reprovado'; -- se ele não pagou é reprovado
 		declare historicoCursor cursor for select cpfAluno, idOferta, condicao from historico; 
 		declare continue handler for not found set done = 1;
 
 		open historicoCursor;
 		repeat
-			fetch historicoCursor into idA, idO, idC;
-			if (idA = idAl and idO = idOf) then 
+			fetch historicoCursor into cpfA, idO, idC;
+			if (cpfA = cpfAl and idO = idOf) then 
 					if (idC = 'aprovado' or idC = 'aprovado por média') then
 						set passadoCondicao = 'aprovado';
 					end if;
